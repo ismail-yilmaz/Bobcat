8c6828e9d97184b6f630c842a6c00e9301661a2a
ismail-yilmaz <ismailyilmaz1978@gmail.com>
2025-11-09 13:26:29 +0300

    Bobcat: Version bumped to 0.9.8

--- Bobcat/Utils.cpp
+++ Bobcat/Utils.cpp
@@ -278,7 +278,7 @@ dword GetAKModifierKey(const KeyInfo& k, int index)
 
 String GetVersion()
 {
-	String ver = Format("%d.%d.%d", 0, 9, 7);
+	String ver = Format("%d.%d.%d", 0, 9, 8);
 
 #ifdef bmGIT_REVCOUNT
 	return ver + " (r" + bmGIT_REVCOUNT + ")";
